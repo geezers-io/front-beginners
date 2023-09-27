9c6400d39e5302276bbe8d612ed2301739c9313e
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-28 01:29:03 +0900

    fix: prop-types 에러 수정

--- src/page/Movie.jsx
+++ src/page/Movie.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardBody, Heading, Stack, Text } from '@chakra-ui/react';
 
 // export default function Movie({ title, poster_path, vote_average }){
 //     return(
@@ -13,24 +14,23 @@ import PropTypes from 'prop-types';
 //     )
 // }
 
-export default function Movie({movies}){
-    return(
-        <Card> 
-            <CarBody>
-                <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} alt="영화 포스터" />
-                <Stack>
-                    <Heading size='md'>{movies.title} </Heading>
-                    <Text> {movies.overview} </Text>
-                    <Text >{movies.vote_average} </Text>
-                </Stack>
-            </CarBody>
-        </Card>
-    )
+export default function Movie({ movie }) {
+  return (
+    <Card>
+      <CardBody>
+        <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="영화 포스터" />
+        <Stack>
+          <Heading size="md">{movie.title}</Heading>
+          <Text> {movie.overview} </Text>
+          <Text>{movie.vote_average} </Text>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
 
-
-MovieCard.propTypes = {
-    movie: PropTypes.shape({
+Movie.propTypes = {
+  movie: PropTypes.shape({
     adult: PropTypes.bool,
     backdrop_path: PropTypes.string,
     genre_ids: PropTypes.arrayOf(PropTypes.number),
@@ -46,5 +46,5 @@ MovieCard.propTypes = {
     video: PropTypes.bool,
     vote_average: PropTypes.number,
     vote_count: PropTypes.number,
-    }),
+  }),
 };
--- src/page/MovieList.jsx
+++ src/page/MovieList.jsx
@@ -1,43 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { getMovies } from '../api/movie';
+import { getMovies } from '@/api/movie';
 import Movie from './Movie';
 
 export default function MovieList() {
-    const [movies, setMovies] = useState({
-        loading: true,
-        list: [],
-    });
+  const [movies, setMovies] = useState({
+    loading: true,
+    list: [],
+  });
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const movies = await getMovies();
-                setMovies(() => ({
-                    loading: false,
-                    list: movies.results,
-                }));
-            } catch (e) {
-                console.error(e);
-            }
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        const movies = await getMovies();
+        setMovies(() => ({
+          loading: false,
+          list: movies.results,
+        }));
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
 
-    if (movies.loading) {
-        return null;
-    }
+  if (movies.loading) {
+    return null;
+  }
 
-    return (
-        <div>
-            <div className='app-container'>
-                {movies.list.map(item => (
-                    <Movie
-                    key={item.title}
-                    title={item.title}
-                    poster_path={item.poster_path}
-                    vote_average={item.vote_average}
-                />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="app-container">
+      {movies.list.map(item => (
+        <Movie key={item.title} movie={item} />
+      ))}
+    </div>
+  );
 }
