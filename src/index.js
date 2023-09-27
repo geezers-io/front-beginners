6d327fb188c2c1c4ee0af62923275f87bf4a41b5
Will Kim <will@chequer.io>
2023-09-27 18:47:13 +0900

    feat: prettier, eslint 룰 설정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div />
