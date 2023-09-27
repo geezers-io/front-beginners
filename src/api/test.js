99be462b659fee23e83bf1171244e0d98297cdc1
Will Kim <will@chequer.io>
2023-09-27 18:57:09 +0900

    fix: lint 커맨드 추가 및 fix, 테스트용 파일 제거

--- src/api/test.js
+++
@@ -1,9 +0,0 @@
-import { getMovie } from './client';
-
-async function test() {
-    const movie = await getMovie(11);
-    console.log({ movie });
-}
-
-
-export default test;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,1 @@
-import react from "react";
-
-
-
-제가 만약에 여기를 바꿨어요 
+export {};
