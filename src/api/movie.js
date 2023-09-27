f259e7f585eca0426e67642078a6be6c64a62638
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-28 01:28:37 +0900

    fix: fix prettier

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,8 +1,7 @@
-import { getMovieApiKey } from "../utils/env";
+import { getMovieApiKey } from '../utils/env';
 const apiKey = getMovieApiKey();
 
 console.log({ apiKey });
 
 export const getMovies = () =>
-    fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
-    .then(data => data.json());
+  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`).then(data => data.json());
