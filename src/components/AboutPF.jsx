a80c6d82f782101380b9a42c917c4e962bfe0d21
Ellsy23 <dlwnsgus3153@naver.com>
2023-10-02 04:43:28 +0900

    feat: styled-aboutpf

--- src/components/AboutPF.jsx
+++ src/components/AboutPF.jsx
@@ -4,13 +4,13 @@ import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const GitIcon = () => {
   return (
-    <Flex alignItems="center" className="about__icon-wrap">
+    <Flex flexDirection="column" alignItems="center" className="about__icon-wrap">
       <img
         className="about__icon"
         src="https://cdn.icon-icons.com/icons2/936/PNG/512/github-logo_icon-icons.com_73546.png"
         alt="Github Icon"
-        w="25px"
-        h="25px"
+        width="25px"
+        height="25px"
       />
     </Flex>
   );
@@ -20,13 +20,14 @@ const AboutPF = () => {
   return (
     <Box
       w="100%"
-      maxWidth="12.5rem"
-      minHeight="9.5rem"
+      maxWidth="25rem"
+      minHeight="8rem"
       display="flex"
       alignItems="center"
       flexDirection="column"
       backgroundColor="#ffffff"
       borderRadius="5px"
+      padding="1.33rem"
     >
       <Flex alignItems="center" className="text-wrap">
         <GitIcon />
@@ -36,11 +37,34 @@ const AboutPF = () => {
         <Link
           href="https://github.com/geezers-io/front-beginners"
           fontSize="1.25rem"
+          fontWeight="bold"
           color="#000000"
           textDecoration="none"
           isExternal
         >
           geezers-io/front-beginners
+          <ExternalLinkIcon />
+        </Link>
+      </Flex>
+
+      <Flex alignItems="center" className="text-wrap">
+        <GitIcon />
+        <Text fontSize="1.25rem" fontWeight="500">
+          Github
+        </Text>
+        <Link href="https://github.com/Ellsy23" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
+          Ellsy23
+          <ExternalLinkIcon />
+        </Link>
+      </Flex>
+
+      <Flex alignItems="center" className="text-wrap">
+        <GitIcon />
+        <Text fontSize="1.25rem" fontWeight="500">
+          Github
+        </Text>
+        <Link href="https://github.com/heejung0413" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
+          heejung0413
           <ExternalLinkIcon />
         </Link>
       </Flex>
