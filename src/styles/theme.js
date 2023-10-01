e553ac5b106ef122373896d4a64570dfd2634167
Ellsy23 <dlwnsgus3153@naver.com>
2023-10-01 21:28:56 +0900

    feat: chakra-variables

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
+const colors = {
+  pink: {
+    primary: '#fe0559',
+    secondary: '#ff7471',
+  },
+  black: '#000000',
+  white: '#ffffff',
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, colors });
 export default theme;
