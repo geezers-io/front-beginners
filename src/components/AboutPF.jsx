f1f32714ead129a0f8840779c448753e76829d2b
Ellsy23 <dlwnsgus3153@naver.com>
2023-10-02 23:35:40 +0900

    feat: about-page-pf

--- src/components/AboutPF.jsx
+++ src/components/AboutPF.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import { Box, Flex, Text, Link, ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  colors: {
+    pink: {
+      primary: '#fe0559',
+      secondary: '#ff7471',
+    },
+    black: '#000000',
+    white: '#ffffff',
+  },
+});
 
 const GitIcon = () => {
   return (
@@ -18,57 +33,59 @@ const GitIcon = () => {
 
 const AboutPF = () => {
   return (
-    <Box
-      w="100%"
-      maxWidth="25rem"
-      minHeight="8rem"
-      display="flex"
-      alignItems="center"
-      flexDirection="column"
-      backgroundColor="#ffffff"
-      borderRadius="5px"
-      padding="1.33rem"
-    >
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link
-          href="https://github.com/geezers-io/front-beginners"
-          fontSize="1.25rem"
-          fontWeight="bold"
-          color="#000000"
-          textDecoration="none"
-          isExternal
-        >
-          geezers-io/front-beginners
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
+    <ChakraProvider theme={theme}>
+      <Box
+        w="100%"
+        maxWidth="25rem"
+        minHeight="8rem"
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        backgroundColor="white"
+        borderRadius="5px"
+        padding="1.33rem"
+      >
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500">
+            Github
+          </Text>
+          <Link
+            href="https://github.com/geezers-io/front-beginners"
+            fontSize="1.25rem"
+            fontWeight="bold"
+            color="black"
+            textDecoration="none"
+            isExternal
+          >
+            geezers-io/front-beginners
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
 
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link href="https://github.com/Ellsy23" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
-          Ellsy23
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500">
+            Github
+          </Text>
+          <Link href="https://github.com/Ellsy23" fontSize="1.25rem" color="black" textDecoration="none" isExternal>
+            Ellsy23
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
 
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link href="https://github.com/heejung0413" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
-          heejung0413
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
-    </Box>
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500">
+            Github
+          </Text>
+          <Link href="https://github.com/heejung0413" fontSize="1.25rem" color="black" textDecoration="none" isExternal>
+            Heejung0413
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 };
 
