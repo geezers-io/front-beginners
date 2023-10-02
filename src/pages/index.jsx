e2e57e07029b0de8e0eddf9ac9606d8f915eb600
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-03 00:12:16 +0900

    feat: 인피니티 스크롤링 기능 작성

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { getMovies } from '@/api/client';
 import MovieCard from '@/components/MovieCard';
 import styled from '@emotion/styled';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const MovieListPage = () => {
   const [movies, setMovies] = useState({
     loading: true,
     list: [],
+    page: 1,
   });
+
+  const movieLength = movies.list.length;
+
+  const getMoreMovieList = async () => {
+    try {
+      const { page, results } = await getMovies(movies.page + 1);
+      setMovies({
+        page,
+        list: movies.list.concat(results),
+        loading: false,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   useEffect(() => {
     (async () => {
       try {
         const movies = await getMovies();
         setMovies(() => ({
+          page: movies.page,
           loading: false,
           list: movies.results,
         }));
@@ -26,12 +44,15 @@ const MovieListPage = () => {
   if (movies.loading) {
     return null;
   }
+
   return (
-    <MovieList>
-      {movies.list.map(item => (
-        <MovieCard key={item.title} movie={item} />
-      ))}
-    </MovieList>
+    <InfiniteScroll next={getMoreMovieList} dataLength={movieLength} loader={null} hasMore>
+      <MovieList>
+        {movies.list.map(item => (
+          <MovieCard key={item.title} movie={item} />
+        ))}
+      </MovieList>
+    </InfiniteScroll>
   );
 };
 
