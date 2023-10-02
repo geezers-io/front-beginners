08c32b6631545dbb506ea3decf39820f7a67f865
Will Kim <will@chequer.io>
2023-10-02 18:07:08 +0900

    refactor: Nav 를 Header 안으로 이동

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,27 +1,34 @@
 import styled from '@emotion/styled';
+import { Tab, TabList, Tabs } from '@chakra-ui/react';
 
 const Header = () => {
   return (
     <SHeader>
       <img src="/EUTCHA.png" width={500} />
+
+      <nav>
+        <Tabs>
+          <TabList>
+            <Tab Link href="/">
+              Home
+            </Tab>
+            <Tab Link href="/">
+              Detail
+            </Tab>
+            <Tab Link href="/">
+              About
+            </Tab>
+          </TabList>
+        </Tabs>
+      </nav>
     </SHeader>
   );
 };
 
 const SHeader = styled.header`
-  margin-top: 0;
   padding: 10px 0;
   margin-bottom: 1.8em;
   background-color: black;
-  z-index: 10;
-  background-position: center;
-
-  img {
-    text-align: center;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
 `;
 
 export default Header;
