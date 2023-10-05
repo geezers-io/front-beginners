66072f030e2849b8171e7b38b3f85524caddca73
heejung <acb4287@naver.com>
2023-10-05 22:57:10 +0900

    feat: infinite-scroll

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { getMovies } from '@/api/client';
 import MovieCard from '@/components/MovieCard';
 import styled from '@emotion/styled';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Flex } from '@chakra-ui/react';
 
 const MovieListPage = () => {
   const [movies, setMovies] = useState({
     loading: true,
     list: [],
+    page: 1,
   });
+
+  const movieLength = movies.list.length;
+
+  const getMoreMovieList = async () => {
+    try {
+      const { page, results } = await getMovies(movies.page + 1);
+      setMovies({
+        page,
+        list: movies.list.concat(results),
+        loading: false,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   useEffect(() => {
     (async () => {
       try {
         const movies = await getMovies();
         setMovies(() => ({
+          page: movies.page,
           loading: false,
           list: movies.results,
         }));
@@ -27,11 +46,13 @@ const MovieListPage = () => {
     return null;
   }
   return (
-    <MovieList>
-      {movies.list.map(item => (
-        <MovieCard key={item.title} movie={item} />
-      ))}
-    </MovieList>
+    <InfiniteScroll next={getMoreMovieList} dataLength={movieLength} loader={null} hasMore>
+      <MovieList>
+        {movies.list.map(item => (
+          <MovieCard key={item.id} movie={item} />
+        ))}
+      </MovieList>
+    </InfiniteScroll>
   );
 };
 
