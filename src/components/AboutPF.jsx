39bd080795ddc4faaa5a0c453c4631a860de493a
heejung <acb4287@naver.com>
2023-10-05 22:11:13 +0900

    fix: text 첨부와 깃허브 주소박스 css 수정

--- src/components/AboutPF.jsx
+++ src/components/AboutPF.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import { Box, Flex, Text, Link, Heading } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const GitIcon = () => {
@@ -9,7 +9,7 @@ const GitIcon = () => {
         className="about__icon"
         src="https://cdn.icon-icons.com/icons2/936/PNG/512/github-logo_icon-icons.com_73546.png"
         alt="Github Icon"
-        width="25px"
+        width="20px"
         height="25px"
       />
     </Flex>
@@ -18,57 +18,68 @@ const GitIcon = () => {
 
 const AboutPF = () => {
   return (
-    <Box
-      w="100%"
-      maxWidth="25rem"
-      minHeight="8rem"
-      display="flex"
-      alignItems="center"
-      flexDirection="column"
-      backgroundColor="#ffffff"
-      borderRadius="5px"
-      padding="1.33rem"
-    >
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link
-          href="https://github.com/geezers-io/front-beginners"
-          fontSize="1.25rem"
-          fontWeight="bold"
-          color="#000000"
-          textDecoration="none"
-          isExternal
-        >
-          geezers-io/front-beginners
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
+    <div>
+      <Heading> 안녕하세요 저희는... </Heading> {''}
+      <Text fontSize="3xl"> 프론트엔드 개발자를 꿈꾸는 새싹들입니다...</Text>
+      <Text fontSize="3xl"> 왓챠피디아를 기반으로 페이지를 제작하였습니다.</Text>
+      <Text fontSize="3xl"> 왓챠피디아를 기반으로 페이지를 제작하였습니다.</Text>
+      <Box
+        w="100%"
+        maxWidth="25rem"
+        minHeight="8rem"
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        backgroundColor="#ffffff"
+        borderRadius="5px"
+        padding="1.33rem"
+      >
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500" color="black">
+            Github{' '}
+          </Text>
+          <Link
+            href="https://github.com/geezers-io/front-beginners"
+            fontSize="1.25rem"
+            color="#000000"
+            textDecoration="none"
+            isExternal
+          >
+            geezers-io/front-beginners
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
 
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link href="https://github.com/Ellsy23" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
-          Ellsy23
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500" color="black">
+            Github
+          </Text>
+          <Link href="https://github.com/Ellsy23" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
+            Ellsy23
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
 
-      <Flex alignItems="center" className="text-wrap">
-        <GitIcon />
-        <Text fontSize="1.25rem" fontWeight="500">
-          Github
-        </Text>
-        <Link href="https://github.com/heejung0413" fontSize="1.25rem" color="#000000" textDecoration="none" isExternal>
-          heejung0413
-          <ExternalLinkIcon />
-        </Link>
-      </Flex>
-    </Box>
+        <Flex alignItems="center" className="text-wrap">
+          <GitIcon />
+          <Text fontSize="1.25rem" fontWeight="500" color="black">
+            Github
+          </Text>
+          <Link
+            href="https://github.com/heejung0413"
+            fontSize="1.25rem"
+            color="#000000"
+            textDecoration="none"
+            isExternal
+          >
+            heejung0413
+            <ExternalLinkIcon />
+          </Link>
+        </Flex>
+      </Box>
+    </div>
   );
 };
 
