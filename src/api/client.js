426f2b8cfc408a08574b4ae1ffd8968b3e5e2db8
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2023-10-06 02:34:35 +0900

    feat: infinite-scroll (#26)
    Co-authored-by: heejung <acb4287@naver.com>
    Co-authored-by: Will Kim <will@chequer.io>

--- src/api/client.js
+++ src/api/client.js
@@ -73,5 +73,29 @@ const API_KEY = process.env.REACT_APP_DB_API;
 export const getMovie = movieId =>
   fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`).then(data => data.json());
 
-export const getMovies = () =>
-  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`).then(data => data.json());
+/**
+ * @description
+ * @param page
+ * @returns
+ * {Promise<{
+ *    page: number;
+ *    results: {
+ *      adult: false;
+ *      backdrop_path: string;
+ *      id: number; title: string;
+ *      original_language: string;
+ *      original_title: string;
+ *      overview: string;
+ *      poster_path: string;
+ *      media_type: string;
+ *      genre_ids: number[];
+ *      popularity: number;
+ *      release_date: string;
+ *      video: boolean;
+ *      vote_average: number;
+ *      vote_count: number;
+ *   }[]
+ * }>}
+ */
+export const getMovies = (page = 1) =>
+  fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&page=${page}`).then(data => data.json());
