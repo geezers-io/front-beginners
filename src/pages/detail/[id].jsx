307b31d462f0e18d650751f84b45dfcaa490cd5a
Will Kim <will@chequer.io>
2023-10-06 23:26:28 +0900

    refactor: detail 리팩터링

--- src/pages/detail/[id].jsx
+++ src/pages/detail/[id].jsx
@@ -1,92 +1,93 @@
 import { useParams } from 'react-router-dom';
 import Trailer from '@/components/Trailer';
 import React, { useEffect, useState } from 'react';
-import { Badge, Box, Center, Flex, Heading, ListItem, Spinner, Text, UnorderedList } from '@chakra-ui/react';
+import { Badge, Box, Center, Flex, Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { getMovie, getMovieVideoInfo } from '@/api/client';
 import Loading from '@/components/Loading';
 
 const Detail = () => {
   const { id } = useParams();
   const [info, setInfo] = useState();
-  const [isLoading, setIsLoading] = useState(true);
   const [trailerKey, setTrailerKey] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getMovieInfo = async id => {
+  const fetchMovieInfo = async id => {
+    const info = await getMovie(id);
+
+    setInfo(info);
+  };
+
+  const fetchMovieTrailer = async id => {
+    const { results } = await getMovieVideoInfo(id);
+
+    const youtubeVideo = results.find(video => video.site === 'YouTube' && video.type === 'Trailer');
+    setTrailerKey(youtubeVideo?.key);
+  };
+
+  const initialize = async movieId => {
     try {
       setIsLoading(true);
 
-      const info = await getMovie(id);
-
-      setInfo(info);
-    } catch (error) {
-      console.error(error);
+      await Promise.all([fetchMovieInfo(movieId), fetchMovieTrailer(movieId)]);
+      console.log('done');
+    } catch (e) {
+      console.error(e);
     } finally {
       setIsLoading(false);
     }
   };
 
-  const fetchMovieTrailer = async id => {
-    try {
-      const videoInfo = await getMovieVideoInfo(id);
-
-      const youtubeVideo = videoInfo.find(video => video.site === 'YouTube' && video.type === 'Trailer');
-      setTrailerKey(youtubeVideo?.key);
-    } catch (error) {
-      console.error('Error fetching movie trailer:', error);
-    }
-  };
-
   useEffect(() => {
-    const movieId = Number(id);
-
-    getMovieInfo(movieId);
-    fetchMovieTrailer(movieId);
+    initialize(Number(id));
   }, []);
 
-  return (
-    <Box className="detail">
-      {isLoading ? (
+  if (isLoading) {
+    return (
+      <Box className="detail">
         <Center height="100vh">
           <Loading />
         </Center>
-      ) : (
-        <Flex>
-          <Box flex="1">
-            <Heading as="h1" size="xl">
-              {info.title}
-            </Heading>
-            <Text fontSize="lg">Year: {info.year}</Text>
-            <Text fontSize="lg">Genres: {info.genres.map(genre => genre.name).join(', ')}</Text>
-            <Text fontSize="lg">Runtime: {info.runtime} minutes</Text>
+      </Box>
+    );
+  }
+  return (
+    <Box className="detail">
+      <Flex>
+        <Box flex="1">
+          <Heading as="h1" size="xl">
+            {info.title}
+          </Heading>
+          <Text fontSize="lg">Year: {info.year}</Text>
+          <Text fontSize="lg">Genres: {info.genres.map(genre => genre.name).join(', ')}</Text>
+          <Text fontSize="lg">Runtime: {info.runtime} minutes</Text>
 
-            <Box className="movie-info">
-              <Flex className="movie-info__summary">
-                <Box className="movie-info__summary__text-wrap">
-                  <Text className="movie-info__title">{info.title}</Text>
-                  <Text className="movie-info__year">{info.release_date}</Text>
-                  {info.runtime !== 0 && <Text className="movie-info__runtime">{info.runtime}</Text>}
+          <Box className="movie-info">
+            <Flex className="movie-info__summary">
+              <Box className="movie-info__summary__text-wrap">
+                <Text className="movie-info__title">{info.title}</Text>
+                <Text className="movie-info__year">{info.release_date}</Text>
+                {info.runtime !== 0 && <Text className="movie-info__runtime">{info.runtime}</Text>}
 
-                  <UnorderedList className="genre-list" listStyleType="none">
-                    {info.genres.map((genre, idx) => (
-                      <ListItem key={idx} className="genre-list__item">
-                        <Badge variant="outline">{genre.name}</Badge>
-                      </ListItem>
-                    ))}
-                  </UnorderedList>
-                </Box>
-              </Flex>
-
-              <Box className="description">
-                <Text>{info.overview && `Official Trailer Video: ${info.overview}`}</Text>
+                <UnorderedList className="genre-list" listStyleType="none">
+                  {info.genres.map((genre, idx) => (
+                    <ListItem key={idx} className="genre-list__item">
+                      <Badge variant="outline">{genre.name}</Badge>
+                    </ListItem>
+                  ))}
+                </UnorderedList>
               </Box>
+            </Flex>
+
+            <Box className="description">
+              <Text>{info.overview && `Official Trailer Video: ${info.overview}`}</Text>
             </Box>
           </Box>
+        </Box>
 
-          <Box flex="1">
-            <Trailer title={info.title} trailerCode={undefined} />
-          </Box>
-        </Flex>
-      )}
+        <Box flex="1">
+          <Trailer title={info.title} trailerCode={trailerKey} />
+        </Box>
+      </Flex>
     </Box>
   );
 };
